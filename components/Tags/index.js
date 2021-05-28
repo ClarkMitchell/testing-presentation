37fe26cbbd46010ec13e52d02efa043e98f86216
ClarkMitchell <clark.mitchell@cbsinteractive.com>
2021-05-28 18:05:31 -0400

    Make component submit on enter.

--- components/Tags/index.js
+++ components/Tags/index.js
@@ -2,10 +2,12 @@ import { useRef, useState } from 'react';
 
 export default function Tags() {
   const inputEl = useRef(null);
-  const [ tags, setTags ] = useState([]);
+  const [tags, setTags] = useState([]);
   const MAXIMUM_TAGS = 3;
 
-  function submit() {
+  function submit(e) {
+    e.preventDefault();
+
     if (tags.length === MAXIMUM_TAGS) {
       return;
     }
@@ -16,17 +18,19 @@ export default function Tags() {
   }
 
   return (
-    <div>
-      <label htmlFor="input">
-        Tags
-        <input type="text" id="input" ref={inputEl} />
-      </label>
-      <button type="button" onClick={submit}>
-        Enter
-      </button>
+    <form className="tags" onSubmit={submit}>
+      <div className="tags__input">
+        <label htmlFor="input">
+          <h1>Tags</h1>
+          <input type="text" id="input" ref={inputEl} />
+        </label>
+      </div>
+      <div className="tags__button">
+        <button type="submit">Enter</button>
+      </div>
       {tags.map((tag) => (
         <span key={tag}>{tag}</span>
       ))}
-    </div>
-  )
+    </form>
+  );
 }
