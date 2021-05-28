7b0ddfe7b6427f47f80ccf06c9016137df236e0c
ClarkMitchell <clark.mitchell@cbsinteractive.com>
2021-05-28 15:08:06 -0400

    Select input by label text.

--- components/Tags/tags.test.js
+++ components/Tags/tags.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import Tags from '~/components/Tags';
 
 describe('The Tags Component', () => {
   it('accepts user input.', () => {
     render(<Tags></Tags>);
+    const input = screen.getByLabelText(/Tags/);
   });
 });
