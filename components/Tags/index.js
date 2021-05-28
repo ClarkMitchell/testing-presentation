c35647ef13401be83f8bce89b1954454c2a2c5e6
ClarkMitchell <clark.mitchell@cbsinteractive.com>
2021-05-28 15:19:00 -0400

    Add ref to track input value, add onClick to clear input, first expectation passes, second fails.

--- components/Tags/index.js
+++ components/Tags/index.js
@@ -1,11 +1,19 @@
+import { useRef } from 'react';
+
 export default function Tags() {
+  const inputEl = useRef(null);
+
+  function submit() {
+    inputEl.current.value = '';
+  }
+
   return (
     <div>
       <label htmlFor="input">
         Tags
-        <input type="text" id="input" />
+        <input type="text" id="input" ref={inputEl} />
       </label>
-      <button type="button">
+      <button type="button" onClick={submit}>
         Enter
       </button>
     </div>
