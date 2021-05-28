46a72cdf6d97851c86a0149926a29b5597c7363e
ClarkMitchell <clark.mitchell@cbsinteractive.com>
2021-05-28 15:06:41 -0400

    Create a test, render a component.

--- components/Tags/index.js
+++ components/Tags/index.js
@@ -1,21 +1,8 @@
-import {useRef} from 'react'
-
 export default function Tags() {
-  const inputEl = useRef(null);
-
-  function submit() {
-    inputEl.current.value = '';
-  }
-
   return (
-    <div>
-      <label htmlFor="input">
-        Tags
-        <input type="text" id="input" ref={inputEl} />
-      </label>
-      <button type="button" onClick={submit}>
-        Enter
-      </button>
-    </div>
-  )
+    <label htmlFor="input">
+      Tags
+      <input type="text" id="input" />
+    </label>
+  );
 }
--- components/Tags/tags.test.js
+++ components/Tags/tags.test.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import { screen, render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Tags from '~/components/Tags';
 
 describe('The Tags Component', () => {
   it('accepts user input.', () => {
     render(<Tags></Tags>);
-    const input = screen.getByLabelText(/Tags/);
-    const value = 'foobar';
-
-    fireEvent.change(input, { target: { value } });
-
-    expect(input.value).toBe(value);
-  });
-
-  it('creates tags from input.', () => {
-    render(<Tags></Tags>);
-    const input = screen.getByLabelText(/Tags/);
-    const value = 'foobar';
-    fireEvent.change(input, { target: { value } });
-
-    const button = screen.getByText(/Enter/);
-    fireEvent.click(button);
-
-    expect(input.value).toBe('');
-    expect(screen.getByText(value)).toBeInTheDocument();
   });
 });
