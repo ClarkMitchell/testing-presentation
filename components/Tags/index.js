34272ed897c7f3d969d98eb432db83420621e276
ClarkMitchell <clark.mitchell@cbsinteractive.com>
2021-05-28 15:04:06 -0400

    :rocket: init

---
+++ components/Tags/index.js
@@ -0,0 +1,21 @@
+import {useRef} from 'react'
+
+export default function Tags() {
+  const inputEl = useRef(null);
+
+  function submit() {
+    inputEl.current.value = '';
+  }
+
+  return (
+    <div>
+      <label htmlFor="input">
+        Tags
+        <input type="text" id="input" ref={inputEl} />
+      </label>
+      <button type="button" onClick={submit}>
+        Enter
+      </button>
+    </div>
+  )
+}
